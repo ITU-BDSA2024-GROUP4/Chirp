91f7d174fcf35aa02fbd63b00f87b883c4802c5a
Allan Petersen <allp@itu.dk>
2024-08-28 15:55:30 +0200

    Added read fucntion
    We added a read method so that we can read the CSV File
    
    co-authored-by: Mikkel <mikcl@itu.dk>
    co-authored-by: Bergur <berd@itu.dk>

---
+++ Chirp.CLI/Program.cs
@@ -0,0 +1,24 @@
+﻿if(args[0] == "read"){
+    try
+    {
+        // Open the text file using a stream reader.
+        using StreamReader reader = new("chirp_cli_db.csv");
+        reader.ReadLine();
+        String line;
+        
+        while ((line = reader.ReadLine()) != null)
+        {
+            string[] values = line.Split('"');
+            string name = values[0].Remove(values[0].Length - 1);
+            DateTimeOffset timestamp = DateTimeOffset.FromUnixTimeSeconds(long.Parse(values[2].Remove(0, 1))).ToLocalTime();
+            string msg = values[1];
+            Console.WriteLine(name + " @ " + timestamp.DateTime + ": " + msg);
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
+    }
+}
+
