37abf8eb3a71d86acf7f7ac74c1a5377b9636493
SforziniVictor <visf@itu.dk>
2024-09-17 21:28:19 +0200

    add program changes

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -3,7 +3,13 @@ using CsvHelper;
 using SimpleDB;
 using DocoptNet;
 
-const string usage = @"Chirp CLI version.
+namespace Chirp.CLI;
+
+public class Program {
+    
+    
+
+    const string usage = @"Chirp CLI version.
 
 Usage:
   chirp read [<limit>]
@@ -16,46 +22,49 @@ Options:
   --version     Show version.
 ";
 
-var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
-var csvDatabase = new CSVDatabase<Cheep>();
-
-if (arguments["read"].IsTrue)
-{
-    try
+    public static void Main(string[] args)
     {
-        IEnumerable<Cheep> cheeps;
-		int limit = arguments["<limit>"].AsInt;
+        var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
+        var csvDatabase = new CSVDatabase<Cheep>();
 
-        if (limit > 1)
+        if (arguments["read"].IsTrue)
         {
-            cheeps = csvDatabase.Read(limit);
+            try
+            {
+                IEnumerable<Cheep> cheeps;
+                int limit = arguments["<limit>"].AsInt;
+
+                if (limit > 1)
+                {
+                    cheeps = csvDatabase.Read(limit);
+                }
+                else
+                {
+                    cheeps = csvDatabase.Read();
+                }
+                var UI = new UserInterface();
+                UI.PrintCheeps(cheeps);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+        else if (arguments["cheep"].IsTrue)
+        {
+            string author = Environment.UserName;
+            DateTimeOffset timestamp = DateTime.UtcNow;
+            string message = arguments["<message>"].ToString();
+
+            csvDatabase.Store(new Cheep(author, message, timestamp.ToUnixTimeSeconds()));
+
+            Console.WriteLine("Cheeped!");
         }
         else
         {
-            cheeps = csvDatabase.Read();
+            Console.WriteLine("Command not recognized!");
         }
-
-		UserInterface.PrintCheeps(cheeps);
-    }
-    catch (IOException e)
-    {
-        Console.WriteLine("The file could not be read:");
-        Console.WriteLine(e.Message);
     }
 }
-else if (arguments["cheep"].IsTrue)
-{
-    string author = Environment.UserName;
-    DateTimeOffset timestamp = DateTime.UtcNow;
-	string message = arguments["<message>"].ToString();
-
-    csvDatabase.Store(new Cheep(author, message, timestamp.ToUnixTimeSeconds()));
-
-    Console.WriteLine("Cheeped!");
-}
-else
-{
-    Console.WriteLine("Command not recognized!");
-}
-
 public record Cheep(string Author, string Message, long Timestamp);
