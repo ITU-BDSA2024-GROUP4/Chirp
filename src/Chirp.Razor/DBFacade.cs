b3e6c22f5ef7cbf6cd8ec1def73b089122eb7074
Victor Sforzini <visf@itu.dk>
2024-10-01 19:34:18 +0200

    add sql integration co-authored-by: Allan <allp@itu.dk> co-authored-by: Mikkel <mickl@itu.dk> co-authored-by: Bergur <berd@itu.dk>

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using System.Diagnostics;
+
 using Microsoft.Data.Sqlite;
 
 namespace Chirp.SQLite;
@@ -19,6 +20,62 @@ public class DBFacade : ICheepService
         }
     }
 
+    public List<CheepViewModel> GetCheeps()
+    {
+        List<CheepViewModel> cheeps = new();
+
+        using (SqliteConnection connection = new($"Data Source={sqlDBFilePath}"))
+        {
+            connection.Open();
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = @"SELECT username, text, pub_date 
+                                    FROM message m JOIN user u ON 
+                                    m.author_id = u.user_id
+                                    ORDER BY m.pub_date DESC;";
+
+            using SqliteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                IDataRecord dataRecord = reader;
+                string username = (string)dataRecord["username"];
+                string text = (string)dataRecord["text"];
+                long pub_date = (long)dataRecord["pub_date"];
+                cheeps.Add(new CheepViewModel(username, text,
+                    CheepService.UnixTimeStampToDateTimeString(pub_date)));
+            }
+        }
+        return cheeps;
+    }
+
+    public List<CheepViewModel> GetCheepsFromAuthor(string author)
+    {
+        List<CheepViewModel> cheeps = new();
+
+        using (SqliteConnection connection = new($"Data Source={sqlDBFilePath}"))
+        {
+            connection.Open();
+
+            SqliteCommand UidCmd = connection.CreateCommand();
+            UidCmd.CommandText = @"SELECT user_id FROM user WHERE username = @author";
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = @"SELECT * FROM message ORDER by message.pub_date desc WHERE author_id = user_id;";
+
+            using SqliteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                IDataRecord dataRecord = reader;
+
+                string text = (string)dataRecord["text"];
+                string pub_date = (string)dataRecord["pub_date"];
+
+                cheeps.Append(new CheepViewModel(author, text, pub_date));
+            }
+        }
+
+        return cheeps;
+    }
+
     private void InitDB()
     {
         try
@@ -29,38 +86,5 @@ public class DBFacade : ICheepService
         {
             Console.WriteLine(e);
         }
-    }
-
-    public List<CheepViewModel> GetCheeps()
-    {
-        List<CheepViewModel> cheeps = new List<CheepViewModel>();
-        
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
-        {
-            connection.Open();
-
-            var command = connection.CreateCommand();
-            command.CommandText = @"SELECT * FROM message ORDER by message.pub_date desc";
-
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                var dataRecord = (IDataRecord)reader;
-                
-                var author  = (string)(dataRecord["author"]);
-                var text = (string)(dataRecord["text"]);
-                var pub_date = (string)(dataRecord["pub_date"]);
-                
-                cheeps.Append(new CheepViewModel(author, text, pub_date));
-            }
-        }
-        
-        return cheeps;
-    }
-
-    public List<CheepViewModel> GetCheepsFromAuthor(string author)
-    {
-        List<CheepViewModel> cheeps = new List<CheepViewModel>();
-        return cheeps;
     }
 }
