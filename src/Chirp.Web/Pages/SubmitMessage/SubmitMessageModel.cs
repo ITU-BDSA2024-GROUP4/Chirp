c6fbedbc92a4b70cf1602fe95ecb5cbef43df8ec
ITU-mikcl <mikcl@itu.dk>
2024-11-12 13:05:27 +0100

    Doesn't work, when html is changed
    co-authored-by: Allan <allp@itu.dk>
    co-authored-by: Bergur <berd@itu.dk>
    co-authored-by: Victor <visf@itu.dk>
    co-authored-by: Lucas <lucasveha@gmail.com>

--- src/Chirp.Web/Pages/SubmitMessage/SubmitMessageModel.cs
+++ src/Chirp.Web/Pages/SubmitMessage/SubmitMessageModel.cs
@@ -1,5 +1,7 @@
 #nullable disable
 
+using System.ComponentModel.DataAnnotations;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace Chirp.Web.Pages;
@@ -7,6 +9,9 @@ namespace Chirp.Web.Pages;
 public class SubmitMessageModel
 {
     [BindProperty]
+    [Required]
+    [StringLength(160, ErrorMessage = "Maximum length is {1}")]
+    [Display(Name = "Message Text")]
     public string Message { get; set; }
 
 }
